ddb105518e1310cb319d8c5bb59efb44b42f55bf
julzedz <edoziechukwuma@gmail.com>
2023-08-08 17:34:14 +0100

    Set up routes

--- app/models/operation.rb
+++ app/models/operation.rb
@@ -1,4 +1,4 @@
 class Operation < ApplicationRecord
-  belongs_to :user, foreign_key: 'author_id'
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_and_belongs_to_many :groups
 end
