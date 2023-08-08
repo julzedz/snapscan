0e23749088f2b7726ead7db3eccedba52d9dae15
julzedz <edoziechukwuma@gmail.com>
2023-08-08 16:49:40 +0100

    Create other models and define associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_many :operations, foreign_key: 'author_id', dependent: :destroy
+  has_many :groups, dependent: :destroy
+
+  validates :name, presence: true
 end
