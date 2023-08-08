b42b23dcda1f4fecb2cf1fdea51393cd69fc58c4
julzedz <edoziechukwuma@gmail.com>
2023-08-08 16:01:26 +0100

    Generate User model

--- db/schema.rb
+++ db/schema.rb
@@ -7,9 +7,6 @@
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
-#
-# It's strongly recommended that you check this file into your version control system.
-
 ActiveRecord::Schema[7.0].define(version: 0) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
