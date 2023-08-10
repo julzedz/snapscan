757cba8fdad4c06af4aa6f014d5025975552d797
julzedz <edoziechukwuma@gmail.com>
2023-08-10 01:12:18 +0100

    Add methods for group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,8 +1,49 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user!
-  load_and_authorize_resource
+  skip_before_action :authenticate_user!, only: :index
+  before_action :set_group, only: %i[show update destroy]
 
   def index
-    @groups = Group.all
+    @user = current_user
+    @groups = current_user.groups.includes(:operations).order(id: :desc)
+  end
+
+  def create
+    add_group = current_user.groups.new(group_params)
+
+    if add_group.save
+      redirect_to groups_path, notice: 'Category was successfully created'
+    else
+      render :new, status: :unprocessable_entity, alert: 'Failed to create category'
+    end
+  end
+
+  def update
+    @group = Group.find(params[:id])
+    if group.update(group_params)
+      redirect_to groups_path, notice: 'Category was successfully updated'
+    else
+      render :edit, alert: 'Failed to update category'
+    end
+  end
+
+  def destroy
+    @user = current_user
+    @group = Group.find(params[:id])
+
+    if @group.destroy
+      redirect_to groups_path, notice: 'Category was successfully removed'
+    else
+      render :index, alert: 'Failed to remove category'
+    end
+  end
+
+  private
+
+  def set_group
+    @group = Group.find(params[:id])
+  end
+
+  def group_params
+    params.permit(:name, :icon)
   end
 end
