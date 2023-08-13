69abaffa1d43b286710c0da1d9dcf5e09a8d5964
julzedz <edoziechukwuma@gmail.com>
2023-08-13 02:46:52 +0100

    Write rspec unit tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable
   has_many :operations, foreign_key: 'author_id', dependent: :destroy
   has_many :groups, dependent: :destroy
 
